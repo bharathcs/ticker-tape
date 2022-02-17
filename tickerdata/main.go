cc9f393187cf34ca8967231b03ede8f003bd54bf
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-02-17 10:21:39 +0800

    Add area and tooltip effect to chart

--- tickerdata/main.go
+++ tickerdata/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	"io"
+	"math"
 	"strconv"
 )
 
@@ -28,11 +29,11 @@ func ReadData(name, ticker, period string, points int, csvData io.Reader) (*Tick
 	data := make([]opts.LineData, 0)
 	xAxisLabels := make([]string, 0)
 	for _, row := range rows[1:] {
-		xAxisLabels = append(xAxisLabels, row[0])
+		xAxisLabels = append([]string{row[0]}, xAxisLabels...)
 		high, _ := strconv.ParseFloat(row[2], 10)
 		low, _ := strconv.ParseFloat(row[3], 10)
 		data = append(data, opts.LineData{
-			Value:  (high + low) / 2.0,
+			Value:  math.Round((high+low)/2.0*100) / 100, // avg of high + low, rounded to 2dp
 			Symbol: ticker,
 		})
 	}
@@ -40,10 +41,10 @@ func ReadData(name, ticker, period string, points int, csvData io.Reader) (*Tick
 	return &TickerData{
 		Name:        name,
 		Ticker:      ticker,
-		Data:        data[0:points],
+		Data:        data[len(data)-points:],
 		Period:      period,
 		Points:      points,
-		XAxisSeries: xAxisLabels,
+		XAxisSeries: xAxisLabels[:points],
 	}, nil
 }
 
@@ -53,7 +54,13 @@ func (t TickerData) CreateLineChart(writer io.Writer) {
 		Title: fmt.Sprintf("%s (%s) - %s (%d)", t.Name, t.Ticker, t.Period, t.Points),
 	}))
 
-	line.SetXAxis(t.XAxisSeries)
-	line.AddSeries(t.Name, t.Data)
+	line.SetXAxis(t.XAxisSeries).
+		AddSeries(t.Name, t.Data).
+		SetGlobalOptions(charts.WithTooltipOpts(opts.Tooltip{
+			Show:      true,
+			Trigger:   "axis",
+			TriggerOn: "mousemove|click",
+		})).
+		SetSeriesOptions(charts.WithAreaStyleOpts(opts.AreaStyle{Opacity: 0.2}))
 	line.Render(writer)
 }
